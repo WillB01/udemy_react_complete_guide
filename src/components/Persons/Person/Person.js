931c431f8a961a7ee1b7db0bb52c829842bcc212
WillB01 <wb.william@outlook.com>
2018-10-01 14:27:43 +0200

    withClass

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,29 +1,27 @@
 import React, {Component} from 'react';
 // import Radium from 'radium';
 import classes from './Person.css';
+import WithClass from '../../../hoc/WithClass';
 
 class Person extends Component {
     constructor(props) {
         super(props);
         console.log('[Person.js] Inside constructor ' + props);
-    
       }
-    
       componentWillMount() {
         console.log('[Person.js] Inside componentWillMount()');
       }
-    
       componentDidMount() {
         console.log('[Person.js] Inside componentDidMount()');
       }
-    render() {
+      render() {
         console.log('[Person.js] Inside render()');
         return(
-            <div className={classes.Person}>
+            <WithClass classes={classes.Person}>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </WithClass>
         );
     }
 }
